db74cbba11b986a2bd676ee3b700e8c5d43610d7
Daniel Ibarra <dannibar201@gmail.com>
2025-02-21 13:13:27 -0500

    added navigation menu using javascript and updated styling

--- index2.js
+++ index2.js
@@ -1,3 +1,13 @@
+
+const burger = document.querySelector(".burger");
+const navmenu = document.querySelector(".nav-menu");
+
+burger.addEventListener("click", () => {
+
+  navmenu.classList.toggle("active");
+
+});
+
 function addBoxShadow(id) {
   const element = document.getElementById(id);
   element.style.boxShadow = "0px 0px 5px 1.5px rgba(0, 0, 0, 0.2)";
